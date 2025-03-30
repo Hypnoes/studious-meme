acf857e6a58376323cf352cbf6e334b86e6b8858
Hypnoes Liu <liumulei@icloud.com>
2025-03-30 15:35:53 +0000

    Add Actix web framework and update devcontainer name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+
+async fn root() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Start the HTTP server
+    HttpServer::new(move || App::new().route("/", web::get().to(root)))
+        .bind(("127.0.0.1", 9000))?
+        .run()
+        .await
 }
